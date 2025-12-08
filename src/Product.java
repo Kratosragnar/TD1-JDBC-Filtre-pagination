68cc39e03c445947c282405650e2907c18f2bad6
JIMMY-Kratos <jimmyheritiana430@gmail.com>
2025-12-08 15:16:22 +0300

    first commit

---
+++ src/Product.java
@@ -0,0 +1,24 @@
+import java.time.Instant;
+
+public class Product {
+    private int id;
+    private String name;
+    private Instant creationDatetime;
+    private Category category;
+
+    public Product(int id, String name, Instant creationDatetime, Category category) {
+        this.id = id;
+        this.name = name;
+        this.creationDatetime = creationDatetime;
+        this.category = category;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public Instant getCreationDatetime() { return creationDatetime; }
+    public Category getCategory() { return category; }
+
+    public String getCategoryName() {
+        return category != null ? category.getName() : null;
+    }
+}
